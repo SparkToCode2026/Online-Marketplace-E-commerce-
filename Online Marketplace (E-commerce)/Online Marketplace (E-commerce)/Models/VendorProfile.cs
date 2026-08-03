c1fa7e7d4b9d10623048808be5e090803f371405
mutaz29 <mutaz6991@gmail.com>
2026-08-04 03:02:08 +0400

    create project structure

---
+++ Online Marketplace (E-commerce)/Online Marketplace (E-commerce)/Models/VendorProfile.cs
@@ -0,0 +1,6 @@
+﻿namespace Online_Marketplace__E_commerce_.Models
+{
+    public class VendorProfile
+    {
+    }
+}
---
+++ Online Marketplace (E-commerce)/Online Marketplace (E-commerce)/ProjectContext.cs
@@ -0,0 +1,6 @@
+﻿namespace Online_Marketplace__E_commerce_
+{
+    public class ProjectContext
+    {
+    }
+}
