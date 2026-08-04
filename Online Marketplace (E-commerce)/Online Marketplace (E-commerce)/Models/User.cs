98b48e21d6c77ad952c64a607831911dc54f20be
mutaz29 <mutaz6991@gmail.com>
2026-08-05 03:02:39 +0400

    Add user Property

--- Online Marketplace (E-commerce)/Online Marketplace (E-commerce)/Models/User.cs
+++ Online Marketplace (E-commerce)/Online Marketplace (E-commerce)/Models/User.cs
@@ -2,5 +2,13 @@
 {
     public class User
     {
+        public int  UserId {  get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string PasswordHash { get; set; }
+        public int Phonenumber { get; set; }
+        public string Role { get; set; } // "Customer" or "Vendor"
+
+
     }
 }
