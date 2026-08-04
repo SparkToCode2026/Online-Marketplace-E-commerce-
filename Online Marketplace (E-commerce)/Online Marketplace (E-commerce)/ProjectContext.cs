215b7262515c3a9a1ee7971d484405097a493196
mutaz29 <mutaz6991@gmail.com>
2026-08-05 03:14:02 +0400

    add Models

--- Online Marketplace (E-commerce)/Online Marketplace (E-commerce)/ProjectContext.cs
+++ Online Marketplace (E-commerce)/Online Marketplace (E-commerce)/ProjectContext.cs
@@ -1,6 +1,12 @@
-﻿namespace Online_Marketplace__E_commerce_
+﻿using Microsoft.EntityFrameworkCore;
+using Online_Marketplace__E_commerce_.Models;
+namespace Online_Marketplace__E_commerce_
 {
     public class ProjectContext
     {
+        public DbSet <User> Users { get; set; }
+        public DbSet<VendorProfile> VendorProfiles { get; set; }
+
+
     }
 }
