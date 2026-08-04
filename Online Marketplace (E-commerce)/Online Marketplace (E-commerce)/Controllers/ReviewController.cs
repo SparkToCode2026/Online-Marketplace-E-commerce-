f81a5ba0221015f91ea9d3edf9624c1abe1671bd
khaildalhadil <khaild.alhadi2020@gmail.com>
2026-08-04 15:42:54 +0400

    add Controllers folder and ReviewController

---
+++ Online Marketplace (E-commerce)/Online Marketplace (E-commerce)/Controllers/ReviewController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Online_Marketplace__E_commerce_.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ReviewController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return Ok("Review Index");
+        }
+    }
+}
