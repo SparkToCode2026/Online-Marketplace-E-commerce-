eb28e3ab419c104c2b7b80eebb67e0f3644aa069
mutaz29 <mutaz6991@gmail.com>
2026-08-05 03:29:50 +0400

    add migrations

--- Online Marketplace (E-commerce)/Online Marketplace (E-commerce)/ProjectContext.cs
+++ Online Marketplace (E-commerce)/Online Marketplace (E-commerce)/ProjectContext.cs
@@ -6,14 +6,15 @@ namespace Online_Marketplace__E_commerce_
     public class ProjectContext : DbContext
     {
         //Models 
-        public DbSet <User> Users { get; set; }
+        public DbSet<User> Users { get; set; }
         public DbSet<VendorProfile> VendorProfiles { get; set; }
 
         // conect to database
         protected override void OnConfiguring(DbContextOptionsBuilder options)
         {
             options.UseSqlServer(
-                "Server=localhost\SQLEXPRESS;Database=EcommerceDB;Trusted_Connection=True;TrustServerCertificate=True;"
+                "Server=localhost\\SQLEXPRESS;Database=EcommerceDB;Trusted_Connection=True;TrustServerCertificate=True;"
             );
         }
+    }
 }
