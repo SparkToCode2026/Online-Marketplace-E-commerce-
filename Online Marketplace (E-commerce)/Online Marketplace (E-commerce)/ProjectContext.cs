4431aebc7537946b66a3d52a8a87a23c06ec37ba
mutaz29 <mutaz6991@gmail.com>
2026-08-05 03:18:47 +0400

    conect to database

--- Online Marketplace (E-commerce)/Online Marketplace (E-commerce)/ProjectContext.cs
+++ Online Marketplace (E-commerce)/Online Marketplace (E-commerce)/ProjectContext.cs
@@ -1,12 +1,19 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Online_Marketplace__E_commerce_.Models;
+
 namespace Online_Marketplace__E_commerce_
 {
-    public class ProjectContext
+    public class ProjectContext : DbContext
     {
+        //Models 
         public DbSet <User> Users { get; set; }
         public DbSet<VendorProfile> VendorProfiles { get; set; }
 
-
-    }
+        // conect to database
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(
+                "Server=localhost\SQLEXPRESS;Database=EcommerceDB;Trusted_Connection=True;TrustServerCertificate=True;"
+            );
+        }
 }
