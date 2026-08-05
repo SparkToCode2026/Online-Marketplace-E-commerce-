5e3a8240a79d5795b4a26328a2620c8bb46e66f6
mutaz29 <mutaz6991@gmail.com>
2026-08-06 02:16:44 +0400

    some update

--- Online Marketplace (E-commerce)/Online Marketplace (E-commerce)/Models/User.cs
+++ Online Marketplace (E-commerce)/Online Marketplace (E-commerce)/Models/User.cs
@@ -1,9 +1,11 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 namespace Online_Marketplace__E_commerce_.Models
 {
     public class User
     {
         [Key]
+        [JsonIgnore]
         public int  UserId {  get; set; }
         public string Username { get; set; }
         public string Email { get; set; }
