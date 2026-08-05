12dd8aa4fe3fe608ee9890e8a7ca346abe6d84d0
mutaz29 <mutaz6991@gmail.com>
2026-08-06 02:56:46 +0400

    add Injected through the constructor

--- Online Marketplace (E-commerce)/Online Marketplace (E-commerce)/Controllers/UserController.cs
+++ Online Marketplace (E-commerce)/Online Marketplace (E-commerce)/Controllers/UserController.cs
@@ -1,7 +1,15 @@
-﻿namespace Online_Marketplace__E_commerce_.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Online_Marketplace__E_commerce_.Controllers
 {
-    public class UserController
+    public class UserController : ControllerBase
     {
+        private readonly ProjectContext _context;
+        public UserController(ProjectContext context)
+        {
+            _context = context;
+        }
+
 
     }
 }
