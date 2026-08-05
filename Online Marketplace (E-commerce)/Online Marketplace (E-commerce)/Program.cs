446a959b94b4405fc309b38f53e4404cacc7acb7
mutaz29 <mutaz6991@gmail.com>
2026-08-05 21:25:51 +0400

    update programcs

--- Online Marketplace (E-commerce)/Online Marketplace (E-commerce)/Program.cs
+++ Online Marketplace (E-commerce)/Online Marketplace (E-commerce)/Program.cs
@@ -1,4 +1,6 @@
 
+using Microsoft.EntityFrameworkCore;
+
 namespace Online_Marketplace__E_commerce_
 {
     public class Program
@@ -8,6 +10,9 @@ namespace Online_Marketplace__E_commerce_
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+
+            builder.Services.AddDbContext<ProjectContext>(options =>
+            options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
             builder.Services.AddControllers();
             
