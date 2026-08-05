d8a31a562b34b73d213ef6446b2babbe40310436
mutaz29 <mutaz6991@gmail.com>
2026-08-06 02:44:14 +0400

    update user model

--- Online Marketplace (E-commerce)/Online Marketplace (E-commerce)/Models/User.cs
+++ Online Marketplace (E-commerce)/Online Marketplace (E-commerce)/Models/User.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text.Json.Serialization;
 namespace Online_Marketplace__E_commerce_.Models
 {
@@ -7,14 +8,22 @@ namespace Online_Marketplace__E_commerce_.Models
         [Key]
         [JsonIgnore]
         public int  UserId {  get; set; }
+        //User display name
+        [Required]
         public string Username { get; set; }
         public string Email { get; set; }
+        [JsonIgnore]
         public string PasswordHash { get; set; }
         public int Phonenumber { get; set; }
         public string Role { get; set; } // "Customer" or "Vendor"
 
-       
 
+        // holds a foreign key back to this user.
+         public bool isActive { get; set; }
+
+        [InverseProperty("user")]
+        [JsonIgnore]
+        public VendorProfile vendorProfile { get; set; }
 
     }
 }
