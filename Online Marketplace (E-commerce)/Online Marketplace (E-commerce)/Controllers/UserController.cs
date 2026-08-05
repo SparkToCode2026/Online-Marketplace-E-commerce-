90dab754c1a8ed44f303de90eb779ac2e9cb94da
mutaz29 <mutaz6991@gmail.com>
2026-08-06 03:04:08 +0400

    add Case 1 Register a User

--- Online Marketplace (E-commerce)/Online Marketplace (E-commerce)/Controllers/UserController.cs
+++ Online Marketplace (E-commerce)/Online Marketplace (E-commerce)/Controllers/UserController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Online_Marketplace__E_commerce_.Models;
 
 namespace Online_Marketplace__E_commerce_.Controllers
 {
@@ -9,7 +11,24 @@ namespace Online_Marketplace__E_commerce_.Controllers
         {
             _context = context;
         }
-
+        // Case 01 — Register a User
+        public  IActionResult AddUser(UserRegister register)
+        {
+            if (_context.Users.Any(u => u.Email == register.email))
+                return BadRequest("Email already registered");
+            User user = new User
+            {
+                Username = register.userName,
+                Email = register.email,
+                PasswordHash = register.password,
+                Phonenumber = 0, // Assuming phone number is optional
+                Role = register.role,
+                isActive = true
+            };
+            _context.Users.Add(user);
+            _context.SaveChanges();
+            return Ok(user.UserId);
+        }
 
     }
 }
