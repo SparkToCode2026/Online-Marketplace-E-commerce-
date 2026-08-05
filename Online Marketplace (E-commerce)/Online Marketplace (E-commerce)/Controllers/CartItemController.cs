60b2f6dd3b443948d3bb42725027e70ecb18a898
nawalsuliman199@gmail.com <nawalsuliman199@gmail.com>
2026-08-06 02:34:56 +0400

    add CartItemController

---
+++ Online Marketplace (E-commerce)/Online Marketplace (E-commerce)/Controllers/CartItemController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Online_Marketplace__E_commerce_.Controllers
+{
+    [Route("cartitem")]
+    [ApiController]
+    public class CartItemController : ControllerBase
+    {
+        private readonly ProjectContext context;
+
+        public CartItemController(ProjectContext context)
+        {
+            this.context = context;
+        }
+    }
+    }
