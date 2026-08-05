16a1ef613f1dee65c1db23a8a0515851251d7360
nawalsuliman199@gmail.com <nawalsuliman199@gmail.com>
2026-08-06 02:07:12 +0400

    Add CartController

---
+++ Online Marketplace (E-commerce)/Online Marketplace (E-commerce)/Controllers/CartController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Online_Marketplace__E_commerce_.Controllers
+{
+    [Route ("cart")]
+    [ApiContoller]
+    public  class CartController : ControllerBase
+    {
+        private readonly ProjectContext context;
+
+        public CartController(ProjectContext context)
+        {
+            this.context = context;
+        }
+    }
+}
