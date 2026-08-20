1bcbfde5fdace2add124b479a968f5380f86b881
Khalid <khaild.alhadi2021@gmail.com>
2026-08-20 19:13:39 +0400

    this is test commit

--- backend/Online Marketplace (E-commerce)/DTOs/CartDto.cs
+++ backend/Online Marketplace (E-commerce)/DTOs/CartDto.cs
@@ -5,7 +5,7 @@ namespace Online_Marketplace__E_commerce_.DTOs
         public int cartId { get; set; }
         public int userId { get; set; }
         public DateTime createdAt { get; set; }
-
+        // test
         public UserDto? user { get; set; }
         public List<CartItemDto>? cartItems { get; set; }
     }
